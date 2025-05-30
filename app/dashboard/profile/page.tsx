a8b0fffa88c4c859b76828d0b96f14c2cacb7be0
James <33164679+jamescolb7@users.noreply.github.com>
2025-05-30 09:48:27 -0400

    Update book URL

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -39,7 +39,7 @@ export default async function Profile() {
 					<Info className="h-4 w-4" />
 					<AlertTitle>Your selection is currently <b>{submitted ? "submitted!" : "not submitted."}</b></AlertTitle>
 					<AlertDescription>
-						{submitted ? <p>You may still make changes until June 13th.</p> : <p>Booking is still open until June 13th. <Link className="font-bold underline" href="/dashboard/book">Submit Now</Link></p>}
+						{submitted ? <p>You may still make changes until June 13th.</p> : <p>Booking is still open until June 13th. <Link className="font-bold underline" href="/dashboard">Submit Now</Link></p>}
 					</AlertDescription>
 				</Alert>
 			</div>
